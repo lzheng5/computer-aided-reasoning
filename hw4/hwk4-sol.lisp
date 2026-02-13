be6df88df51be87065a3a55d2aaed2b876eb6ec0
lzheng5 <lingxiaozheng515@gmail.com>
2026-02-12 20:35:38 -0500

    q1

--- hw4/hwk4-sol.lisp
+++ hw4/hwk4-sol.lisp
@@ -8,7 +8,6 @@
 (modeling-admit-all)
 
 ;; Q1
-
 (definec m-bad-app (x y :tl acc :all) :nat
    (match (list x y)
      ((nil nil) 0)
@@ -17,47 +16,65 @@
      (& (+ 2 (len x) (len acc)))))
 
 (property bad-app-1 (x y acc :tl)
-    (implies (and (consp x)
-                  (endp y))
-     (< (m-bad-app y x acc)
-        (m-bad-app x y acc))))
+  (implies
+   (and (consp x)
+        (endp y))
+   (< (m-bad-app y x acc)
+      (m-bad-app x y acc))))
 
 (property bad-app-2 (x y acc :tl)
-    (implies (and (endp x)
-                  (consp y))
-     (< (m-bad-app x (rest y) (cons (first y) acc))
-        (m-bad-app x y acc))))
+  (implies
+   (and (endp x)
+        (consp y))
+   (< (m-bad-app x (rest y) (cons (first y) acc))
+      (m-bad-app x y acc))))
 
 (property bad-app-3 (x y acc :tl)
-    (implies (and (consp x)
-                  (consp y))
-     (< (m-bad-app x nil (m-bad-app acc nil y))
-        (m-bad-app x y acc))))
+  (implies
+   (and (consp x)
+        (consp y))
+   (< (m-bad-app x nil (m-bad-app acc nil y))
+      (m-bad-app x y acc))))
 
+(property bad-app-4 (x y acc :tl)
+  (implies
+   (and (consp x)
+        (consp y))
+   (< (m-bad-app acc nil y)
+      (m-bad-app x y acc))))
+
+(set-termination-method :measure)
 (definec bad-app (x y acc :tl) :tl
-   (declare (xargs :measure (m-bad-app x y acc)
-                   :hints (("Goal" :use (bad-app-1 bad-app-2 bad-app-3)))))
+  (declare (xargs :measure (m-bad-app x y acc)))
   (match (list x y)
     ((nil nil) acc)
     ((& nil) (bad-app y x acc))
     ((nil (f . r)) (bad-app x r (cons f acc)))
     (& (bad-app x nil (bad-app acc nil y)))))
 
-(property bad-app-nil-nil (acc :tl)
-    (== (bad-app nil nil acc) acc))
+(property bad-app-nil-nil-acc (acc :tl)
+  (== (bad-app nil nil acc) acc))
 
-(property bad-app-nil (x acc :tl)
+(property bad-app-x-nil-acc (x acc :tl)
   (implies (consp x)
            (== (bad-app x nil acc)
                (bad-app nil x acc))))
 
-(property (y :tl)
-    (== (bad-app nil y acc)
-     (append (rev y) acc))
-  :hints (("goal" :induct (tlp y))))
+(property bad-app-nil-y-acc (x y acc :tl)
+  (implies (endp x)
+           (== (bad-app x y acc)
+               (append (rev y) acc)))
+    :hints (("goal" :induct (bad-app x y acc))))
 
-(property (x y :tl)
-    (== (bad-app x y nil)
-     (if (endp x)
-         (rev y)
-         (app (rev x) y))))
+(property bad-app-cons-y-nil (x y acc :tl)
+  (implies (and (consp x)
+                (endp acc))
+           (== (bad-app x y acc)
+               (append (rev x) y)))
+    :hints (("goal" :induct (bad-app x y acc))))
+
+(property bad-app-x-y-nil (x y :tl)
+  (== (bad-app x y nil)
+      (if (endp x)
+          (rev y)
+          (append (rev x) y))))
