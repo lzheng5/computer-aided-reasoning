dfd105765e6ea390485cef9351beaaf4c8708132
Ling Zheng <lingxiaozheng515@gmail.com>
2026-02-13 15:52:10 -0500

    snapshot

--- hw4/hwk4-sol.lisp
+++ hw4/hwk4-sol.lisp
@@ -9,6 +9,11 @@
 (modeling-admit-all)
 
 ;; Q1
+;; Q1a
+;; Page !!!
+
+
+;; Q1b
 (definec m-bad-app (x y :tl acc :all)
   :nat
   (match (list x y)
@@ -17,6 +22,7 @@
     ((nil (& . &)) (len y))
     (& (+ 2 (len x) (len acc)))))
 
+;; Q1c
 (property bad-app-1 (x y acc :tl)
   (implies
    (and (consp x)
@@ -54,6 +60,7 @@
     ((nil (f . r)) (bad-app x r (cons f acc)))
     (& (bad-app x nil (bad-app acc nil y)))))
 
+;; Q1d
 (property bad-app-nil-nil-acc (acc :tl)
   (== (bad-app nil nil acc) acc))
 
@@ -84,12 +91,15 @@
 
 ;; Q2
 
+;; Q2a
 ;; Page 128
 
+;; Q2b
 (definec m-ack (n :nat m :all)
   :lex
   (if (natp m) (list n m) n))
 
+;; Q2c
 (property m-ack-1 (n m :nat)
     (implies
      (and (not (zp n))
@@ -126,6 +136,10 @@
 (defdata norm-if-expr (or if-atom (list 'if if-atom norm-if-expr norm-if-expr)))
 (defdata-subtype-strict norm-if-expr if-expr)
 
+;; Q3a
+;; Page !!!
+
+;; Q3b
 ;; Consulted Claude
 (definec m-if-flat (x :if-expr) :pos
   (match x
@@ -134,6 +148,7 @@
      (* (m-if-flat a)
         (+ 1 (m-if-flat b) (m-if-flat c))))))
 
+;; Q3c
 (property m-if-flat-1 (x :if-expr)
   (implies
    (and (consp x)
@@ -195,3 +210,101 @@
        (:if-atom `(if ,a ,(if-flat b) ,(if-flat c)))
        (('if d e f)
         (if-flat `(if ,d (if ,e ,b ,c) (if ,f ,b ,c))))))))
+
+;; Q3d
+(defdata if-assign (alistof var bool))
+
+; Notice that if var is not in the if-assign, we return nil.
+(definec lookup-var (var :var a :if-assign) :bool
+  (match a
+    (nil nil)
+    (((!var . val) . &) val)
+    (& (lookup-var var (cdr a)))))
+
+(definec lookup-atom (e :if-atom a :if-assign) :bool
+  (match e
+    (:bool e)
+    (& (lookup-var e a))))
+
+(definec if-eval (e :if-expr a :if-assign) :bool
+  (match e
+    (:if-atom (lookup-atom e a))
+    (('if x y z)
+     (if (if-eval x a) (if-eval y a) (if-eval z a)))))
+
+;; how come this type of goal showed up and cannot be refuted automatically ???
+(property bad-if-eval (x y :all a :if-assign)
+  (implies (and (not (consp x))
+                (if-exprp (list* 'if x y))
+                (consp y)
+                (consp (cdr y))
+                (not (cddr y))
+                (not (booleanp x))
+                (not (varp x))
+                (if-assignp a))
+           (not (if-eval (list* 'if x y) a))))
+
+(property if-flat-equal-if (x :if-expr a :if-assign)
+  (== (if-eval (if-flat x) a) (if-eval x a))
+  :hints (("goal" :induct (if-flat x))))
+
+;; Q3e
+(definec assignedp (e :if-atom a :if-assign) :bool
+  (or (boolp e)
+      (consp (assoc-equal e a))))
+
+(definec validp (e :norm-if-expr a :if-assign) :bool
+  (match e
+    (:if-atom (lookup-atom e a))
+    (('if x y z)
+     (if (assignedp x a)
+         (if (lookup-atom x a)
+             (validp y a)
+             (validp z a))
+       (and (validp y (acons x t a))
+            (validp z (acons x nil a)))))))
+
+(definec check-validp (e :if-expr) :bool
+  (validp (if-flat e) nil))
+
+(property validp-sound-var (x :norm-if-expr a :if-assign)
+  (implies
+   (and (if-atomp x)
+        (lookup-atom x a))
+   (== (validp x a) (if-eval x a))))
+
+(definec agrees-with (b a :if-assign) :bool
+   (match a
+     (nil t)
+     (((v . val) . rst)
+      (and (== (lookup-var v b) val)
+           (agrees-with b rst)))))
+
+(property agrees-with-lookup (a b :if-assign x :if-atom)
+  (implies
+   (and (agrees-with b a)
+        (assignedp x a))
+   (== (lookup-atom x a)
+       (lookup-atom x b))))
+
+(property agrees-with-acons (a b :if-assign x :var v :bool)
+  (implies
+   (and (agrees-with b (acons x v a))
+        (== (lookup-atom x b) v))
+   (agrees-with b a)))
+
+(property agrees-with-acons-2 (a b :if-assign x :var v :bool)
+  (implies
+   (and (agrees-with b a)
+        (== (lookup-atom x b) v))
+   (agrees-with b (acons x v a))))
+
+(property validp-sound (e :norm-if-expr a b :if-assign)
+  (implies
+   (and (validp e a)
+        (agrees-with b a))
+   (if-eval e b))
+  :hints (("goal" :induct (validp e a))))
+
+(property check-validp-is-sound (e :if-expr a :if-assign)
+  (implies (check-validp e) (if-eval e a)))
